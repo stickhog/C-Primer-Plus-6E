b513dcf4caca7392d69da9864f415b48dd084260
stickhog <stickhog69@icloud.com>
2015-12-15 12:11:39 -0800

    Initial Commit

---
+++ C-Primer-Plus-6E/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C-Primer-Plus-6E
+//
+//  Created by Howard Au on 12/15/15.
+//  Copyright © 2015 Howard Au. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
