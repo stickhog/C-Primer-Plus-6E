5f1e1c8bb3befbbed979f8d22531e0594b9d7693
stickhog <stickhog69@icloud.com>
2015-12-18 00:21:03 -0800

    added first.c

---
+++ C-Primer-Plus-6E/ch2_first.c
@@ -0,0 +1,20 @@
+//
+//  ch2_first.c
+//  C-Primer-Plus-6E
+//  pg.28
+//  Created by Howard Au on 12/17/15.
+//  Copyright © 2015 Howard Au. All rights reserved.
+//
+
+#include <stdio.h>
+int main(void)
+{
+    int num;
+    num = 1;
+    
+    printf("I am a simple ");
+    printf("computer.\n");
+    printf("My favorite number is %d because it is first.\n", num);
+    
+    return 0;
+}
